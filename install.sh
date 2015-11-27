1b602da61690a109dec9fec255277d98eda35bcc
yoshiro4444 <massugu.massugu@gmail.com>
2015-11-27 22:36:17 +0900

    ダウンロード処理追加

--- install.sh
+++ install.sh
@@ -1,12 +1,40 @@
 #!/bin/bash
 
-DOT_FILES=( .vimrc .gvimrc .bashrc .zshrc)
+DOTPATH=~/.dotfiles
+GITHUB_URL=https://github.com/yoshiro4460/dotfiles.git
 
+if has "git"; then
+  git clone --recursive "$GITHUB_URL" "$DOTPATH"
 
-for file in ${DOT_FILES[@]}
+  elif has "curl" || has "wget"; then
+    tarball="https://github.com/yoshiro4460/dotfiles/archive/master.tar.gz"
+
+    if has "curl"; then
+      curl -L "$tarball"
+
+    elif has "wget"; then
+      wget -O - "$tarball"
+
+    fi | tar xv -
+
+    mv -f dotfiles-master "$DOTPATH"
+
+  else
+    die "curl or wget required"
+fi
+
+cd ~/.dotfiles
+if [ $? -ne 0 ]; then
+  die "not found: $DOTPATH"
+fi
+
+for file in .??*
 do
 
-  ln -s $HOME/dotfiles/$file $HOME/$file
+  [ "$file" = ".git" ] && continue
+  [ "$file" = ".vimrc.*" ] && continue
+
+  ln -snfv "$DOTPATH/$file" "$HOME/$file"
 
 done
 
