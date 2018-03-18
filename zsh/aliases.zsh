30126c79b2769ea42a928018fb49773a34c7bbb0
m-yoshiro <massugu.massugu@gmail.com>
2018-03-18 16:28:04 +0900

    Add git alias. status=>st, checkout=>co, commit=>co, B = search branch by peco

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -4,3 +4,7 @@ alias relogin='exec $SHELL -l'
 alias chrome="/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome"
 alias chrome-canary="/Applications/Google\ Chrome\ Canary.app/Contents/MacOS/Google\ Chrome\ Canary"
 alias chromium="/Applications/Chromium.app/Contents/MacOS/Chromium"
+
+# Git
+alias g='git'
+alias -g B='`git branch -a --sort=authordate | peco --prompt "GIT BRANCH>" | tr -d " \\*"`'
