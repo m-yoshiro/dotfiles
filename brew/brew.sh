1ad5e717a11cb95d7b783b774b1d87223e0d361a
m-yoshiro <massugu.massugu@gmail.com>
2021-01-02 12:44:22 +0900

    Check before excuting brew

--- brew/brew.sh
+++ brew/brew.sh
@@ -3,13 +3,16 @@
 DOTFILES_ROOT=~/dotfiles
 CURRNET_DIR=`pwd`
 
-which brew >/dev/null 2>&1 && brew doctor
-which brew >/dev/null 2>&1 && brew update
+if ! command -v brew >/dev/null 2>&1; then
+  echo "brew command could not be found"
+  exit
+fi
 
+brew doctor
+brew update
 brew upgrade --all
 
 brew install rcmdnk/file/brew-file
 ln -snfv $CURRENT_DIR/Brewfile $HOME/.config/brewfile/Brewfile
 
 brew file install
-
