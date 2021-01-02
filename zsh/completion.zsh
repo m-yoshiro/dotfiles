b68cd4cd67c218e93e83f04952dc77db1b82486c
m-yoshiro <massugu.massugu@gmail.com>
2021-01-02 19:43:18 +0900

    comment out

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -1,7 +1,7 @@
 # https://docs.brew.sh/Shell-Completion#configuring-completions-in-zsh
-if type brew &>/dev/null; then
-  FPATH=$(brew --prefix)/share/zsh/site-functions:$FPATH
+# if type brew &>/dev/null; then
+#   FPATH=$(brew --prefix)/share/zsh/site-functions:$FPATH
 
-  autoload -Uz compinit
-  compinit
-fi
+#   autoload -Uz compinit
+#   compinit
+# fi
