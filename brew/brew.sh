e2e66af63558f89f75c76cc8c5d5f721bdc982e7
m-yoshiro <massugu.massugu@gmail.com>
2021-02-14 12:12:15 +0900

    fix current dir

--- brew/brew.sh
+++ brew/brew.sh
@@ -1,13 +1,16 @@
 #! /bin/bash
 
-DOTFILES_ROOT=~/dotfiles
-CURRNET_DIR=`pwd`
+link_brewfile() {
+  local current_dir
+  current_dir=$(dirname "${BASH_SOURCE[0]:-$0}")
 
-if ! command -v brew >/dev/null 2>&1; then
-  echo "brew command could not be found"
-  exit
-fi
+  if ! command -v brew >/dev/null 2>&1; then
+    echo "brew command could not be found"
+    exit
+  fi
 
-ln -s $CURRENT_DIR/.Brewfile $HOME/.Brewfile
+  ln -s $current_dir/.Brewfile $HOME/.Brewfile
+}
 
+link_brewfile
 brew bundle
