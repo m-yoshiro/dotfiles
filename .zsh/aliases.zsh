ffe73970aae4b3fbc3695564a2c1f6c96e6a754b
m-yoshiro <massugu.massugu@gmail.com>
2024-12-08 20:43:03 +0900

    Refactor tmux session handling into reusable functions

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -11,31 +11,36 @@ alias -g B='`git branch -a | peco --prompt "GIT BRANCH>" | tr -d " \\*"`'
 
 alias ghbr='gh browse -R $(ghq list | peco)'
 
+switch_or_create_tmux_session() {
+  local dir=$1
+  if [[ ! -z ${TMUX} ]]; then
+    # convert to "organization/repository"
+    repository=$(echo $dir | sed -E 's/.*\/(.*\/.*)$/\1/')
+    session=${repository//./-}
+    current_session=$(tmux list-sessions | grep 'attached' | cut -d":" -f1)
+
+    if [[ $current_session =~ ^[0-9]+$ ]]; then
+      cd $dir
+      tmux rename-session $session
+    else
+      tmux list-sessions | cut -d":" -f1 | grep $session > /dev/null
+      if [[ $? != 0 ]]; then
+        tmux new-session -d -c $dir -s $session
+      fi
+      tmux switch-client -t $session
+    fi
+  else
+    cd $dir
+  fi
+}
+
 # https://qiita.com/yuki_ycino/items/ef5e6b63c8f9af2f03c0#ghq-%E3%81%A8-fzf-%E3%81%A7%E3%81%AE%E3%83%97%E3%83%AD%E3%82%B8%E3%82%A7%E3%82%AF%E3%83%88%E3%81%AE%E7%AE%A1%E7%90%86%E3%81%A8%E7%A7%BB%E5%8B%95
 function select-branch-with-tmux() {
   local dir repository session current_session
   dir=$(ghq root)/$(ghq list | peco --prompt 'GIT BRANCH>')
 
   if [[ $dir != "$(ghq root)/" ]]; then
-    if [[ ! -z ${TMUX} ]]; then
-      # convert to "organization/repository"
-      repository=$(echo $dir | sed -E 's/.*\/(.*\/.*)$/\1/')
-      session=${repository//./-}
-      current_session=$(tmux list-sessions | grep 'attached' | cut -d":" -f1)
-
-      if [[ $current_session =~ ^[0-9]+$ ]]; then
-        cd $dir
-        tmux rename-session $session
-      else
-        tmux list-sessions | cut -d":" -f1 | grep $session > /dev/null
-        if [[ $? != 0 ]]; then
-          tmux new-session -d -c $dir -s $session
-        fi
-        tmux switch-client -t $session
-      fi
-    else
-      cd $dir
-    fi
+    switch_or_create_tmux_session "$dir"
   fi
 }
 
@@ -87,6 +92,20 @@ function git-check-unrefs-files() {
   done
 }
 alias gcuf=git-check-unrefs-files
+
+# goto dotfile dir with tmux
+function goto-dotfile-with-tmux() {
+  local dir repository session current_session
+  dir=$DOTFILES
+
+  if [[ ! -d $dir ]]; then
+    echo "Not found $dir"
+    return
+  fi
+
+  switch_or_create_tmux_session "$dir"
+}
+alias gdot=goto-dotfile-with-tmux
 
 # pnpm
 alias pn=pnpm
